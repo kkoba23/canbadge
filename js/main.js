c6bbb7363ca228bddc46e6f8d60509c95624093b
kkoba23 <kobayashi@k-relations.com>
2017-01-25 21:52:15 +0900

    Signed-off-by: kkoba23 <kobayashi@k-relations.com>

--- js/main.js
+++ js/main.js
@@ -1,53 +1,16 @@
 $(function() {
-
-	// 初期設定
-	fileUpload();
-
-
-});
-//$('#cropBtn').on('click', function () {
-//    var cropImg = $('#image').cropper('getCroppedCanvas', {
-//        width: 350,
-//        height: 350,
-//    });
-//    var cropURI = cropImg.toDataURL("image/jpeg");
-//    $('#cropBtn a').attr('href',cropURI);
-////    $('#cropImg').attr('src',cropURI);
-//    $('#cropImg').html(cropImg);
-//});
-
-
-function fileUpload() {
-
-	$('.filedownImg').on('click', function () {
-	    var cropImg = $('#image').cropper('getCroppedCanvas', {
-	        width: 700,height: 700,
-	    });
-	    var cropURI = cropImg.toDataURL("image/jpeg");
-	    $('#cropBtn a').attr('href',cropURI);
-	});
-
-
-	$(".fileupImg").on("click", function(event) {
-		$("input[name=img]").trigger("click");
-	});
-	$('input[name=img]').change(function() {
-
-		if (!this.files.length) {
-			return;
-		}
-		var file = this.files[0];
-		upload(file);
-
-	});
+	var status=0;
 	// ドラッグ＆ドロップの実装
 	$('.dragArea').on('drop', function(e) {
 		e.preventDefault();
 		$('.dragArea').removeClass('over');
+		$('.dragArea').addClass('dropped');
+		$('#dropBtn').css({display:'inline-block'})
+		$('.dragArea').html('');
+		// $('.dragArea').toggle();
 		// ファイル情報を取得
 		var files = e.originalEvent.dataTransfer.files;
 		upload(files[0]);
-
 	}).on('dragenter', function() {
 		$('.dragArea').addClass('over');
 		return false;
@@ -59,6 +22,71 @@ function fileUpload() {
 		return false;
 	});
 
+	$('#dropBtn').on('drop', function(e) {
+		e.preventDefault();
+		$('#dropBtn').removeClass('over');
+		$('#dropBtn').addClass('dropped');
+		var files = e.originalEvent.dataTransfer.files;
+		upload(files[0]);
+	}).on('dragenter', function() {
+		$('#dropBtn').addClass('over');
+		return false;
+	}).on('dragover', function() {
+		$('#dropBtn').addClass('over');
+		return false;
+	}).on('dragleave', function() {
+		$('#dropBtn').removeClass('over');
+		return false;
+	});
+
+	// 画像をアップロードボタン
+	$(".fileupImg").on("click", function(event) {
+		$("input[name=img]").trigger("click");
+	});
+	// ドラッグエリアの表示/非表示
+	$(".fileupImg").on('mouseover', function() {
+		$('.dragArea').css({ display:'none'});
+	}).on('mouseleave', function(){
+		$('.dragArea').css({ display:'inline'});
+	});
+
+	// 決定ボタン
+	$('.filedownImg').on('click', function() {
+		if(status==0){
+			var cropImg = $('#image').cropper('getCroppedCanvas', {
+				width : 700,
+				height : 700,
+			});
+			$('.hyoji').attr('src', cropImg.toDataURL());
+			$('.hyoji').css({visibility:'visible'});
+			$('body').css({background:'#2df'});
+			$('.cropper-bg').css({visibility:'hidden'});
+			$('.fileupImg').css({visibility:'hidden'});
+			$('#dropBtn').css({visibility:'hidden'});
+			$('.filedownImg').html('やり直す');
+			status=1;
+//			var cropURI = cropImg.toDataURL("image/jpeg");
+//			$('#cropBtn a').attr('href', cropURI);
+		} else {
+			status=0;
+			$('body').css({background:'#0af'});
+			$('#dropBtn').css({visibility:'visible'});
+			$('.fileupImg').css({visibility:'visible'});
+			$('.cropper-bg').css({visibility:'visible'});
+			$('.hyoji').css({visibility:'hidden'});
+			$('.filedownImg').html('決定');
+		}
+	});
+
+	// アップロードボタンを押した時の処理
+	$('input[name=img]').change(function() {
+		if (!this.files.length) {
+			return;
+		}
+		var file = this.files[0];
+		upload(file);
+	});
+
 	// HTMLに表示
 	function upload(file) {
 		var $img = $(".thumb"), $downfile = $(".filedownImg")
@@ -68,4 +96,4 @@ function fileUpload() {
 		};
 		fileReader.readAsDataURL(file);
 	}
-}
+});
