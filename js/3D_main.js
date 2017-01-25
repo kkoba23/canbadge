c7897b8d4933ee2c8b3b40e9fa0b953dfe0bfb94
kkoba23 <kobayashi@k-relations.com>
2017-01-26 01:51:39 +0900

    Signed-off-by: kkoba23 <kobayashi@k-relations.com>

--- js/3D_main.js
+++ js/3D_main.js
@@ -2,6 +2,9 @@
 var scene = new THREE.Scene();
 var canB = new THREE.Object3D();
 var canD = new THREE.Object3D();
+// カメラ
+var camera = new THREE.PerspectiveCamera(50, 360 / 360, 1, 1000);
+
 
 var canbadge3Dinit = function(canTexture) {
 
@@ -14,10 +17,6 @@ var canbadge3Dinit = function(canTexture) {
 	document.getElementById("viewArea").appendChild(renderer.domElement);// 描画領域の実装
 	// document.body.appendChild(renderer.domElement);
 
-	// カメラ
-	var camera = new THREE.PerspectiveCamera(50, 360 / 360, 1, 1000);
-	camera.position.set(0, 0, 35);
-
 	// マウスコントロール
 	var controls = new THREE.OrbitControls(camera);
 
@@ -29,6 +28,11 @@ var canbadge3Dinit = function(canTexture) {
 	var directionalLight2 = new THREE.DirectionalLight('#aaaaaa', 0.5);
 	directionalLight2.position.set(10, -5, 3);
 	scene.add(directionalLight2);
+
+	var directionalLight2 = new THREE.DirectionalLight('#aaaaaa', 0.5);
+	directionalLight2.position.set(-10, -5, -3);
+	scene.add(directionalLight2);
+
 
 	var ambientLight = new THREE.AmbientLight('#aaaaaa', 1.5);
 	scene.add(ambientLight);
@@ -56,8 +60,8 @@ var canbadge3Dinit = function(canTexture) {
 		json.scale.x = 1;
 		json.scale.y = 1;
 		json.scale.z = 1;
-		json.rotation.x = Math.PI / 2;
-		json.rotation.z = -20 / 360 * 2 * Math.PI;
+//		json.rotation.x = Math.PI / 2;
+//		json.rotation.z = -20 / 360 * 2 * Math.PI;
 		canD.add(json);
 	});
 
@@ -66,16 +70,28 @@ var canbadge3Dinit = function(canTexture) {
 	scene.add(canB);
 	scene.add(canD);
 
+	positionInit();
+
 	// レンダリング
 	function render() {
 		requestAnimationFrame(render);
 		// cube.rotation.x += 0.01;
-		canB.rotation.y -= 0.001;
-		canD.rotation.y -= 0.001;
+		canB.rotation.z += 0.001;
+		canD.rotation.z += 0.001;
 		controls.update();
 		renderer.render(scene, camera);
 	}
 	render();
+}
+
+var positionInit = function(){
+	camera.position.set(0, 0, 35);
+	canB.rotation.x = Math.PI / 2;
+	canB.rotation.z = -20 / 360 * 2 * Math.PI;
+	canB.rotation.y = -5 / 360 * 2 * Math.PI;
+	canD.rotation.x = Math.PI / 2;
+	canD.rotation.z = -20 / 360 * 2 * Math.PI;
+	canD.rotation.y = -5 / 360 * 2 * Math.PI;
 }
 
 var canbadgeLoader = function(canTexture) {
@@ -101,9 +117,9 @@ var canbadgeLoader = function(canTexture) {
 		json.scale.x = 1;
 		json.scale.y = 1;
 		json.scale.z = 1;
-		json.rotation.x = Math.PI / 2;
-		json.rotation.z = -20 / 360 * 2 * Math.PI;
-		json.rotation.y = -3 / 360 * 2 * Math.PI;
+//		json.rotation.x = Math.PI / 2;
+//		json.rotation.z = -20 / 360 * 2 * Math.PI;
+//		json.rotation.y = -3 / 360 * 2 * Math.PI;
 		canB.add(json);
 	});
 }
